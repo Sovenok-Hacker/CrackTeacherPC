75b7b89b7b804e7531ff79df0ae155937d0edf47
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-06-29 14:13:14 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ token = 'token'
 bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=['text'])
 def handle(message):
-    if message.text.startswith('get'):
-        filename = ''.join(message.text.split('get '))
+    if message.text.startswith('get '):
+        filename = message.replace('get ', '', 1)
         bot.send_document(message.from_user.id, (filename, open(filename, 'rb').read()))
     elif message.text == 'ip':
         bot.send_message(message.from_user.id, requests.get('https://ifconfig.me/ip').text)
